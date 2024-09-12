9fe6095a5f3755560a1702392af8af45616ec3e1
KanesKaewchui <kaneskaewchui1@gmail.com>
2024-09-12 14:18:31 +0700

    first commi

---
+++ script.js
@@ -0,0 +1,39 @@
+const ownerName = document.querySelector('#owner-name')
+const ownerNameShow = document.querySelector('#owner-name-show')
+
+const cardNo = document.querySelector('#card-no')
+const cardNoShow = document.querySelector('#card-no-show')
+
+const cardExpire = document.querySelector('#card-expire')
+const cardExpireShow = document.querySelector('#card-expire-show')
+
+const cvcCode = document.querySelector('#cvc-code')
+const cvcCodeShow = document.querySelector('#cvc-code-show')
+
+ownerName.addEventListener('input', (e) => {2
+  ownerNameShow.textContent = e.target.value
+})
+
+cardNo.addEventListener('input', (e) => {
+  var format = e.target.value.trim().split("").map((data, index) => {
+    if ((index + 1) % 4 == 0) {
+      data = data + " "
+    }
+    return data
+  })
+  cardNoShow.textContent = format.join("")
+})
+
+cardExpire.addEventListener('input', (e) => {
+  var format = e.target.value.trim().split("").map((data, index) => {
+    if ((index + 1) % 2 == 0 && index + 1 !== 4) {
+      data = data + "/"
+    }
+    return data
+  })
+  cardExpireShow.textContent = format.join("")
+})
+
+cvcCode.addEventListener('input', (e) => {
+  cvcCodeShow.textContent = e.target.value
+})
